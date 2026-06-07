c4f19f67972be204a50c2c6e71b8fef34945a1f2
angkykurniawan <kurniawan.angang2005@gmail.com>
2026-06-07 15:53:31 +0700

    Update Minoritas

--- app/Http/Controllers/PemilikController.php
+++ app/Http/Controllers/PemilikController.php
@@ -19,7 +19,7 @@ class PemilikController extends Controller
         $search = $request->get('search');
 
         // Menambahkan pagination dan opsi per_page
-        $perPage = $request->get('per_page', 2); // Default 10 per halaman
+        $perPage = $request->get('per_page', 2); 
 
         // Jika ada pencarian, filter berdasarkan nama
         if ($search) {
