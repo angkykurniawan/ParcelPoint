bd8656f37f7f46f50096252527a581a46a4abf81
angkykurniawan <kurniawan.angang2005@gmail.com>
2026-06-07 17:50:28 +0700

    Update Prevent Action

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -8,10 +8,16 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PreventBackHistory
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
     public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
 
+        // Membersihkan cache browser agar tombol back tidak menampilkan data lama
         return $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
                         ->header('Pragma', 'no-cache')
                         ->header('Expires', 'Sun, 02 Jan 1990 00:00:00 GMT');
