98a6aec0101e3266ad2e0352042dc0eed1945433
angkykurniawan <kurniawan.angang2005@gmail.com>
2026-06-07 11:39:52 +0700

    Tambah Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin ParcelPoint',
+            'email' => 'admin@pcr.ac.id',
+            'password' => Hash::make('ABC#123'), // Ganti password ini
+            'role' => 'admin',
         ]);
     }
 }
